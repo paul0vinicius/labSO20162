a060e8256cc4b89288c04d9eaf2aa0cf35294fc7
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2016-11-22 14:39:31 -0300

    Tratamento da entrada funcional.
    O xeu recebe comandos com, ou sem, parâmetros. Atualmente só existem o comando date e o comando sleep.

--- LAB02SO20162/sleep.sh
+++ LAB02SO20162/sleep.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-date
+sleep $1
--- LAB02SO20162/xeu_parte1_hugoG_PauloV.c
+++ LAB02SO20162/xeu_parte1_hugoG_PauloV.c
@@ -3,35 +3,62 @@
 #include <string.h>
 #include <unistd.h>
 
+#define MAX_SIZE_USERNAME 20
+
 int main() {
 	char *line = NULL;
-	char buf[20] = "";
-	char * const a[1] = {NULL};
-	char * const b[2] = {NULL, NULL};
+	char username[MAX_SIZE_USERNAME] = "";
+	char *parameters[] = {};
+	char *system_variables[] = {};
 	size_t linecapp = 0;
+
+	//Verificar como definir esse babado aqui pra facilitar a leitura, etc
+	char *command = *parameters[0];
+
+	int i = 0;
+
+	getlogin_r(username, MAX_SIZE_USERNAME);
 	
-	getlogin_r(buf, 20);
 	while(1){
-		printf("%s => ", buf);
+		printf("%s => ", username);
 		getline(&line, &linecapp, stdin);
+		
 		pid_t pid = fork();
-
+		
 		if(pid == 0){ //child
-			printf("%d", strcmp(line, "date\n"));
-			if (strcmp(line, "date\n") == 0){
-				execve("./date.sh", b, a);
-		}
-		} else if (pid > 0) { // parent
+
+			// Modularizar isso para uma função
+			char *p = strtok(line, " ");
+		
+			while(p != NULL){
+				parameters[i] = p;
+				i++;
+				p = strtok(NULL, " ");
+			}
+			//
+
+			// Será que tem como transformar em um case?
+			if (strcmp(parameters[0], "date\n") == 0){
+				execve("./date.sh", parameters, system_variables);
+			} else if (strcmp(parameters[0], "sleep") == 0){
+				execve("./sleep.sh", parameters, system_variables);
+			} else printf("Command not found\n");
+
+			// Modularizar isso para uma função
+			int j = 0;
+			while(j < i){
+				parameters[j] = NULL;
+				j++;
+			}
+			//
+		
+		}else if (pid > 0) { // parent
 			wait();
-		}  else { // fork failed
+		}else{ // fork failed
 			printf("fork() failed!\n");
-        return 1;
-    }
-
-
-			
-			
+        	return 1;
 		}
+	}
 
  return 0;
  
