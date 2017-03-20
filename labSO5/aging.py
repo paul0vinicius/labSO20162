fc0946efab3f8983355fe98c5805efaeaaad56dc
Hugo Gabriel Bezerra da Silva 114110448 <hugogbs@lcc1-25.lcc.ufcg.edu.br>
2017-03-20 17:29:26 -0300

    Declaração dos métodos a serem implementados e adicão do exemplo de implementação

--- labSO5/aging.py
+++ labSO5/aging.py
@@ -9,3 +9,25 @@ class aging:
 			if clock():
 				if referenced(page_id):
 					negocio 
+
+
+  def put(self, frameId):
+    """Allocates this frameId for some page"""
+    # Notice that in the physical memory we don't care about the pageId, we only
+    # care about the fact we were requested to allocate a certain frameId
+    pass
+
+  def evict(self):
+    """Deallocates a frame from the physical memory and returns its frameId"""
+    # You may assume the physical memory is FULL so we need space!
+    # Your code must decide which frame to return, according to the algorithm
+    pass
+
+  def clock(self):
+    """The amount of time we set for the clock has passed, so this is called"""
+    # Clear the reference bits (and/or whatever else you think you must do...)
+    pass
+
+  def access(self, frameId, isWrite):
+    """A frameId was accessed for read/write (if write, isWrite=True)"""
+    pass
--- labSO5/fifo.py
+++ labSO5/fifo.py
@@ -10,3 +10,33 @@ class fifo:
 	
 		
 		
+  def put(self, frameId):
+    """Allocates this frameId for some page"""
+    # Notice that in the physical memory we don't care about the pageId, we only
+    # care about the fact we were requested to allocate a certain frameId
+    
+    return put(frame_id)
+    
+    pass
+
+  def evict(self):
+    """Deallocates a frame from the physical memory and returns its frameId"""
+    # You may assume the physical memory is FULL so we need space!
+    # Your code must decide which frame to return, according to the algorithm
+    
+    return self.fifo.pop(0)
+    
+    pass
+
+  def clock(self):
+    """The amount of time we set for the clock has passed, so this is called"""
+    # Clear the reference bits (and/or whatever else you think you must do...)
+    
+    # Do nothing
+    
+    pass
+
+  def access(self, frameId, isWrite):
+    """A frameId was accessed for read/write (if write, isWrite=True)"""
+    # Do nothing
+    pass
